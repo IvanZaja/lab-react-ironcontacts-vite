fdfb31530faa013a677af706e63f0ba029a3a203
Ivan Zajaczkowski <sayonbra@gmail.com>
2024-04-06 19:45:06 +0200

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,46 @@ function App() {
     setRemainingContacts(updateRemaining);
   }
 
+  const sortName = () => {
+    const updateContacts = [...contacts.sort(function (a, b) {
+      if (a.name > b.name) {
+        return 1;
+      }
+      if (a.name < b.name) {
+        return -1;
+      }
+    })];
+    setContacts(updateContacts);
+    console.log(contacts)
+  }
+
+  const sortPopularity = () => {
+    const updateContacts = [...contacts.sort(function (a, b) {
+      if (a.popularity > b.popularity) {
+        return -1
+      }
+      if (a.popularity < b.popularity) {
+        return 1
+      }
+    })]
+    setContacts(updateContacts);
+    console.log(contacts)
+  }
+
+  const deleteContact = (contact) => {
+    setContacts([...contacts.filter((c) => c.id !== contact.id)])
+    console.log(contacts)
+  }
+
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
-      <button onClick={() => addRandomContact()}>Add random contact</button>
+      <div>
+        <button onClick={() => addRandomContact()}>Add random contact</button>
+        <button onClick={() => sortName()}>Sort by name</button>
+        <button onClick={() => sortPopularity()}>Sort by popularity</button>
+      </div>
+
       <table>
         <thead>
           <tr>
@@ -36,16 +72,17 @@ function App() {
         </thead>
 
         <tbody>
-          {contacts.map((eachContact, i) => {
+          {contacts.map((contact, i) => {
             return (
               <tr key={i}>
                 <td>
-                  <img className="image" src={eachContact.pictureUrl}/>
+                  <img className="image" src={contact.pictureUrl}/>
                 </td>
-                <td>{eachContact.name}</td>
-                <td>{eachContact.popularity.toFixed(2)}</td>
-                {eachContact.wonOscar ? <td><i className="fa-solid fa-trophy"></i></td> : <td></td>}
-                {eachContact.wonEmmy ? <td><i className="fa-solid fa-star"></i></td> : <td></td>}
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+                {contact.wonOscar ? <td><i className="fa-solid fa-trophy"></i></td> : <td></td>}
+                {contact.wonEmmy ? <td><i className="fa-solid fa-star"></i></td> : <td></td>}
+                <td><button onClick={() => deleteContact(contact)}>Delete</button></td>
               </tr>
             )
           })}
